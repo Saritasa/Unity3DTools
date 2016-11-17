a31e7b3726ecfc80ec016a5524d1e79470667111
Sergey Kondratov <s.kondratov@saritasa.com>
2016-11-17 12:44:10 +0700

    Add GameObject guard class

---
+++ Scripts/Utils/GameObjectGuard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    /// <summary>
+    /// Auxiliary class which allows to create temporary objects in scope
+    /// </summary>
+    public class GameObjectGuard : IDisposable
+    {
+        public GameObject Obj { get; private set; }
+
+        public GameObjectGuard(GameObject gameObject)
+        {
+            Obj = gameObject;
+        }
+
+        public void Dispose()
+        {
+            if (Obj == null)
+                return;
+
+            GameObject.Destroy(Obj);
+            Obj = null;
+        }
+    }
+}
