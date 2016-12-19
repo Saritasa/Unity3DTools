3a2e91edca92f3d0a754440293d1e93d34afcc54
Alexander Arbuznikov <arbuznikov@saritasa.com>
2016-12-19 17:30:48 +0700

    Fix spelling errors and code style. Add comments.

--- Editor/FindReferencesInProject/FindReferencesInProject.cs
+++ Editor/FindReferencesInProject/FindReferencesInProject.cs
@@ -4,15 +4,27 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// This utility allows to see what assets, objects or scenes rely(depends) on the selected object(s). 
+/// So, for example if you have a huge project and want to know if you can remove asset from the project or not, you can use this tool.
+/// </summary>
 public class FindReferencesInProject : EditorWindow
 {
+
+    private const string ProgressBarTitle = "Find references in the project.";
+
+    /// <summary>
+    /// List of objects that we are going to collect dependencies of.
+    /// </summary>
     [SerializeField]
-    public List<Object> ObjectsToExamine;
+    private List<Object> ObjectsToExamine;
 
-    //Object that we are looking dependecies for
-    public Object objectToExamine;
+    /// <summary>
+    /// Object that we are looking dependecies for
+    /// </summary>
+    private Object objectToExamine;
 
-    private const string dummyFileName = "FindReferencesInProject.txt";
+    private const string DummyFileName = "FindReferencesInProject.txt";
 
     /// <summary>
     /// A list of found references for the object, including scenes
@@ -24,8 +36,14 @@ public class FindReferencesInProject : EditorWindow
     /// </summary>
     private Vector2 scrollPos = new Vector2();
 
+    /// <summary>
+    /// If true, DependenciesCache for project assets is used, so it takes less time to collect all sependencies.
+    /// </summary>
     private bool fastMode;
 
+    /// <summary>
+    /// Per object dependencies cache.
+    /// </summary>
     private static Dictionary<Object, List<Object>> DependenciesCache;
 
     [MenuItem("Assets/Find References In Project", false, 20)]
@@ -43,18 +61,21 @@ public class FindReferencesInProject : EditorWindow
     [MenuItem("Assets/Find References In Project", true)]
     static bool Validate()
     {
-        //Validate if we selected any asset and not scene object
+        // Validate if we selected any asset and not scene object
         return (Selection.activeObject != null && AssetDatabase.IsMainAsset(Selection.activeObject)); 
     }
 
+    /// <summary>
+    /// There is some bug with Unity UI. In some projects after the script, all texts in the editor are missing. So, we reimport the dummy file to make Editor redraw all it's UI.
+    /// </summary>
     void BugWorkAround()
     {
         // TODO: Remove copy-paste
-        if (!File.Exists(Application.dataPath.Replace("Assets", Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this))) + "/" + dummyFileName)))
+        if (!File.Exists(Application.dataPath.Replace("Assets", Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this))) + "/" + DummyFileName)))
         {
-            File.Create(Application.dataPath.Replace("Assets", Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this))) + "/" + dummyFileName)).Close();
+            File.Create(Application.dataPath.Replace("Assets", Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this))) + "/" + DummyFileName)).Close();
         }
-        AssetDatabase.ImportAsset(Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this))) + "/" + dummyFileName); //Used for removing some bug with fonts (In some projects after the script all texts in the editor are missing by some rison) Some editor redraw bug. After reimporting any asset everything becomes normal.
+        AssetDatabase.ImportAsset(Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this))) + "/" + DummyFileName);
     }
 
     void OnGUI()
@@ -122,91 +143,74 @@ public class FindReferencesInProject : EditorWindow
         EditorGUILayout.EndScrollView();
     }
 
-    public static List<Object> CollectDependencies(List<Object> b)
-    {
-        HashSet<Object> result = new HashSet<Object>();
-        string[] AssetPaths = AssetDatabase.GetAllAssetPaths();
-        List<Object> objs = new List<Object>();
-        foreach (Object objToTest in b)
-            objs.Add(objToTest);
-        Object[] dependencies = EditorUtility.CollectDependencies(objs.ToArray());
-        int i = 0;
-        foreach (string assetPath in AssetPaths)
-        {
-            i++;
-            Object[] assets = AssetDatabase.LoadAllAssetsAtPath(assetPath);
-            foreach (Object asset in assets)
-                if (dependencies.Contains(asset))
-                    result.Add(asset);
-            if (EditorUtility.DisplayCancelableProgressBar("Find references in the project.", "Looking for references in " + AssetPaths.Length + " assets...", (float)i / AssetPaths.Length))
-                break;
-        }
-        return result.ToList();
-    }
-
     /// <summary>
-    /// TODO: Add comment
+    /// Reverse collection of object(s) dependencies. This function allows to get a list of all objects(assets) that depend on selected object(s).
     /// </summary>
-    /// <param name="b"></param>
+    /// <param name="selectedObjects">Objects that we collect dependencies for.</param>
     /// <returns></returns>
-    public static List<Object> CollectReverseDependencies(List<Object> b)
+    public static List<Object> CollectReverseDependencies(List<Object> selectedObjects)
     {
         HashSet<Object> result = new HashSet<Object>();
-        int i = 0;
-        string[] AssetPaths = AssetDatabase.GetAllAssetPaths();
-        List<Object> objs = new List<Object>();
-        foreach (Object objToTest in b)
-            objs.AddRange(AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(objToTest)));
 
-        foreach (string assetPath in AssetPaths)
+        // objectsCollectFrom coluld contain prefabs that contain other objects, so we have to collect all subobjects either.
+        List<Object> selectedObjectsWithChildren = new List<Object>();
+        foreach (Object selectedObject in selectedObjects)
+            selectedObjectsWithChildren.AddRange(AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(selectedObject)));
+
+        string[] AllAssetsPaths = AssetDatabase.GetAllAssetPaths();
+        int i = 0;
+        foreach (string assetPath in AllAssetsPaths)
         {
             i++;
-            Object obj = AssetDatabase.LoadMainAssetAtPath(assetPath);
+            Object asset = AssetDatabase.LoadMainAssetAtPath(assetPath);
 
-            if (b.Contains(obj)) continue;
-            Object[] dependencies = EditorUtility.CollectDependencies(new Object[1] { obj });
-            if (dependencies.Length < 2) continue;
+            // Skip the asset if it is object that we want to collect dependecies of.
+            if (selectedObjects.Contains(asset)) continue;
 
-            foreach (Object dp in dependencies)
-                foreach(Object child in objs)
-                    if (dp == child)
+            Object[] assetDependencies = EditorUtility.CollectDependencies(new Object[1] { asset });
+            // If dependencies count < 2, the the object has only dependency on itself and we can skip it.
+            if (assetDependencies.Length < 2) continue;
+
+            foreach (Object dependency in assetDependencies)
+                foreach(Object child in selectedObjectsWithChildren)
+                    if (dependency == child)
                     {
-                        result.Add(obj);
+                        result.Add(asset);
                         break;
                     }
 
-            if (EditorUtility.DisplayCancelableProgressBar("Find references in the project.", "Looking for references in " + AssetPaths.Length + " assets...", (float)i / AssetPaths.Length))
+            if (EditorUtility.DisplayCancelableProgressBar(ProgressBarTitle, "Looking for references in " + AllAssetsPaths.Length + " assets...", (float)i / AllAssetsPaths.Length))
                 break;
         }
+
         EditorUtility.ClearProgressBar();
         return result.ToList();
     }
 
     /// <summary>
-    /// TODO: Add comments
-    /// TODO: Rename method parameter
+    /// Reverse collection of object(s) dependencies using all assets dependency cache.
     /// </summary>
-    /// <param name="b"></param>
+    /// <param name="selectedObjects"></param>
     /// <returns></returns>
-    public static List<Object> CollectReverseDependenciesFast(List<Object> b)
+    public static List<Object> CollectReverseDependenciesFast(List<Object> selectedObjects)
     {
         if (DependenciesCache == null)
             CacheDependencies();
 
         HashSet<Object> result = new HashSet<Object>();
+
+        List<Object> selectedObjectsWithChildren = new List<Object>();
+        foreach (Object objToTest in selectedObjects)
+            selectedObjectsWithChildren.AddRange(AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(objToTest)));
+
         int i = 0;
-
-        List<Object> objs = new List<Object>();
-        foreach (Object objToTest in b)
-            objs.AddRange(AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(objToTest)));
-
         foreach (KeyValuePair<Object, List<Object>> pair in DependenciesCache)
         {
             i++;
-            if (b.Contains(pair.Key)) continue;
+            if (selectedObjects.Contains(pair.Key)) continue;
             if (pair.Value.Count < 2) continue;
             foreach (Object dp in pair.Value)
-                foreach(Object child in objs)
+                foreach(Object child in selectedObjectsWithChildren)
                     if (dp == child)
                     {
                         result.Add(pair.Key);
@@ -220,7 +224,7 @@ public class FindReferencesInProject : EditorWindow
     }
 
     /// <summary>
-    /// TODO: Add comment
+    /// Create all assets dependency cache.
     /// </summary>
     public static void CacheDependencies()
     {
@@ -234,7 +238,6 @@ public class FindReferencesInProject : EditorWindow
         foreach (string assetPath in AssetPaths)
         {
             i++;
-
             Object obj = AssetDatabase.LoadMainAssetAtPath(assetPath);
             Object[] dependencies = EditorUtility.CollectDependencies(new Object[1] { obj });
             if (dependencies.Length < 2) continue;
